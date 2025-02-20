7120a58170426671917710b73707cbe0da8e328c
Guru7135 <gurumoorthy@vjhsoftware.in>
2025-02-20 18:14:14 +0530

    second commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL(`file://${path.join(__dirname, 'dist/angular-electron-app/index.html')}`);
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
